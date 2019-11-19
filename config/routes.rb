a53339d6eeee38292aa6861f80dcebe12117a17f
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-11-19 15:38:37 +0000

    added stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'groups#index'
+  root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   resources :users
   resources :groups do
